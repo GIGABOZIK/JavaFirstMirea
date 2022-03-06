d0b610288d5a1b06b83cc7b5790eb4491e77bd82
GIGABOZIK <ivan_belyaev_2002@mail.ru>
2022-03-06 15:27:29 +0300

    mmm lexer ;3

--- src/razrpop/DSL/flexer/Lexer.java
+++ src/razrpop/DSL/flexer/Lexer.java
@@ -2,9 +2,6 @@ package razrpop.DSL.flexer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -15,52 +12,104 @@ public class Lexer {
     private static final LinkedHashMap<String, Pattern> tokens = new LinkedHashMap<String, Pattern>();
 
     static {
+        // Константы: числа/строки ( 2 0.11 "Текстик да" )
+        // Имена переменных \ функций: VAR FUNC_NAME
+        // Ключевые слова: KW double float int string do if else while for and or break True False print
+        // Знаки операций: + - =
+        // Разделители: ; [] ,
         //
-        tokens.put("COMMENT", Pattern.compile("#.*")); // *
+        tokens.put("OL_COMMENT", Pattern.compile("#.*")); // *
+        // multi_line_comment // *
 
-        tokens.put("KW_FOR", Pattern.compile("for")); // *
+//        tokens.put("SPACE", Pattern.compile("\s+")); // *
+
+        // KW_VAR
+        tokens.put("KW_BEGIN", Pattern.compile("begin")); // *
+        tokens.put("KW_END", Pattern.compile("end")); // *
         tokens.put("KW_WHILE", Pattern.compile("while")); // *
+        tokens.put("KW_DO", Pattern.compile("do")); // *
         tokens.put("KW_IF", Pattern.compile("if")); // *
+        tokens.put("KW_THEN", Pattern.compile("then")); // *
+        tokens.put("KW_ELSE", Pattern.compile("else")); // *
+        tokens.put("KW_FOR", Pattern.compile("for")); // *
+        // to // *
+        tokens.put("KW_READ", Pattern.compile("read")); // *
+        tokens.put("KW_WRITE", Pattern.compile("write")); // *
+        // KW BOOL,INT,STRING..
+        tokens.put("KW_LOGIC_AND", Pattern.compile("and")); // *
+        tokens.put("KW_LOGIC_OR", Pattern.compile("or")); // *
 
-        tokens.put("VAR", Pattern.compile("[a-zA-Z][a-zA-Z0-9]*"));
-        //tokens.put("FUNC_NAME", Pattern.compile("f" + tokens.get("VAR").toString())); // хз пока
+        tokens.put("KW_LOGIC_TRUE", Pattern.compile("True")); // *
+        tokens.put("KW_LOGIC_FALSE", Pattern.compile("False")); // *
 
-//        tokens.put("SPACE", Pattern.compile("[\s\n]")); // *
+        tokens.put("IDENT", Pattern.compile("[a-z]([a-zA-Z\\d])*")); //
+        tokens.put("INT", Pattern.compile("0|(-?[1-9](\\d)*)")); //
+        tokens.put("STRING", Pattern.compile("\"(.*)\"")); //
+//        tokens.put("STRING", Pattern.compile("\"[\\s0-9a-zA-Z!@#№$;%^:&?*()\\-_=+\\\\|/\\[\\]{}<>']*\"")); //
+        tokens.put("FUNC_NAME", Pattern.compile("[a-zA-Z][a-zA-Z0-9]*\\([.]*\\)")); // хз пока
 
-        tokens.put("L_BRACKET", Pattern.compile("\\(")); // 1 cnt
-        tokens.put("R_BRACKET", Pattern.compile("\\)")); // 2 cnt
-        tokens.put("L_BRACE", Pattern.compile("\\{")); // 1 cnt
-        tokens.put("R_BRACE", Pattern.compile("}")); // 2 cnt
+        // COMMENT
 
-        tokens.put("MATH_OP", Pattern.compile("\\+-|\\*{1,2}|/|\\^|%")); // 1
-        tokens.put("COMPARISON", Pattern.compile("<=?|>=?|==|!=")); // 1
-        tokens.put("ASSIGN_OP", Pattern.compile("=")); // 2
+        tokens.put("KW_VAR", Pattern.compile("Var")); // *
+        tokens.put("KW_BOOL", Pattern.compile("Bool")); // *
+        tokens.put("KW_INT", Pattern.compile("Int")); // *
+        tokens.put("KW_STRING", Pattern.compile("String")); // *
 
-//        tokens.put("STRING", Pattern.compile("\"[\\s0-9a-zA-Z!@#№$;%^:&?*()\\-_=+\\\\|/\\[\\]{}<>']*\"")); // 1
-        tokens.put("QUOTE", Pattern.compile("['\"]")); // 2
+//        tokens.put("MATH_OP", Pattern.compile("\\+-|\\*{1,2}|/|\\^|%")); // 1
 
-//        tokens.put("END_LINE", Pattern.compile(";3")); // 1
-        tokens.put("INT", Pattern.compile("0|(-?[1-9][0-9]*)")); // 2
+        // Доступ через указатель //
+        tokens.put("POW_OP", Pattern.compile("\\*\\*")); //
+        tokens.put("MUL_OP", Pattern.compile("\\*")); //
+        tokens.put("DIV_OP", Pattern.compile("/")); //
+        tokens.put("REM_OP", Pattern.compile("%")); //
+        // Преобразование типа //
+        tokens.put("ADD_OP", Pattern.compile("\\+")); //
+        tokens.put("SUB_OP", Pattern.compile("-")); //
+        // Сдвиг влево \ вправо в 2 с.с. //
+        tokens.put("COMP_VAL", Pattern.compile("<=?|>=?")); //
+        tokens.put("COMP_EQL", Pattern.compile("==|!=")); //
+        tokens.put("B_AND", Pattern.compile("&")); //
+        tokens.put("B_XOR", Pattern.compile("\\^")); //
+        tokens.put("B_OR", Pattern.compile("\\|")); //
+        tokens.put("LOGIC_AND", Pattern.compile("&&")); //
+        tokens.put("LOGIC_OR", Pattern.compile("\\|\\|")); //
+        // cond ? (a_if_true) : (b_if_false) //
+
+        tokens.put("ASSIGN_OP", Pattern.compile("=")); //
+
+        tokens.put("SEP_END_LINE", Pattern.compile("(;3)")); // * хз
+//        tokens.put("SEP_END_LINE", Pattern.compile("(;\\))")); // * хз
+
+        tokens.put("SEP_L_BRACKET", Pattern.compile("\\(")); // cnt
+        tokens.put("SEP_R_BRACKET", Pattern.compile("\\)")); // cnt
+        tokens.put("SEP_L_BRACE", Pattern.compile("\\{")); // cnt
+        tokens.put("SEP_R_BRACE", Pattern.compile("}")); // cnt
+        tokens.put("SEP_SEMICOLON", Pattern.compile(";\s")); //
+        tokens.put("SEP_COLON", Pattern.compile(":")); //
+        tokens.put("SEP_COMMA", Pattern.compile(",")); //
+        tokens.put("SEP_DOT", Pattern.compile("\\.")); //
+        tokens.put("SEP_QUOTE", Pattern.compile("['\"]")); // хз
         //
     }
 
+    private LinkedList<Token> tokList = new LinkedList<>();
+
     public static void main(String[] args) {
         LinkedList<String> strList = new LinkedList<>();
-//        Lexer lxr = new Lexer();
-        LinkedList<Token> tokList = new LinkedList<>();
+        Lexer lxr = new Lexer();
+//        LinkedList<Token> tokList = new LinkedList<>();
 
         // Ввод
-//        Scanner sc = new Scanner(System.in);
-
-        //* Ввод из файла
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new File("src/razrpop/dsl/flexer/somecode.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        //*
-
+            // Подключение консоли
+//            Scanner sc = new Scanner(System.in);
+            // Подключение файла
+            Scanner sc = null;
+            try {
+                sc = new Scanner(new File("src/razrpop/dsl/flexer/somecode.txt"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //
         System.out.println("Enter smth");
         for (int i = 0; true; i++) {
             strList.add(i, sc.nextLine());
@@ -69,12 +118,12 @@ public class Lexer {
                 break;
             }
         }
-        System.out.println("Your input:\n " + strList);
+        System.out.println("Your input:\n " + strList); // Проверка ввода
         if (strList.isEmpty()) return;
 
         //* Проверка порядка токенов
-//        System.out.println(tokens);
-//        for (Token t : tokList) System.out.println(t);
+        System.out.println(tokens);
+//        for (Token t : lxr.tokList) System.out.println(t);
         //*
 
         // Обработка введенных данных
@@ -117,25 +166,53 @@ public class Lexer {
             //*
 
             //* Обработка строки #5
-            for (String tokName : tokens.keySet()) {
-                Pattern rgxPtn = tokens.get(tokName);
-                Matcher rgxMtr = rgxPtn.matcher(strG);
-//                System.out.println("Token - " + tokName + ":");
-                while (rgxMtr.find()) {
-                    String tokValue = strG.substring(rgxMtr.start(), rgxMtr.end());
-                    Token newTok = new Token(tokName, tokValue);
-                    tokList.add(newTok);
-                    System.out.println(newTok);
-                }
-            }
+//            for (String tokName : tokens.keySet()) {
+//                Pattern rgxPtn = tokens.get(tokName);
+//                Matcher rgxMtr = rgxPtn.matcher(strG);
+////                System.out.println("Token - " + tokName + ":");
+//                while (rgxMtr.find()) {
+//                    String tokValue = strG.substring(rgxMtr.start(), rgxMtr.end());
+//                    Token newTok = new Token(tokName, tokValue);
+//                    tokList.add(newTok);
+//                    System.out.println(newTok);
+//                }
+//            }
+//            System.out.println("EOL");
+            //*
+            //* Обработка строки #6
+            strProcessor(lxr, strG);
             System.out.println("EOL");
-            //*
-            //*
+//          //*
         }
         System.out.println("\nRESULT:\n");
-        for (Token t : tokList) System.out.println(t);
+        for (Token token : lxr.tokList) System.out.println(token);
+    }
+
+    private static void strProcessor (Lexer lxr, String strG) {
+        if (strG.isEmpty()) return;
+        int pos = 0;
+        int tokCnt = 0;
+        for (String tokName : tokens.keySet()) {
+            Pattern rgxPtn = tokens.get(tokName);
+            Matcher rgxMtr = rgxPtn.matcher(strG);
+            if (rgxMtr.find()) {
+                if (rgxMtr.start() == pos) {
+                    tokCnt++;
+                    pos = rgxMtr.end();
+                    String tokValue = strG.substring(rgxMtr.start(), rgxMtr.end());
+//                    Token newTok = new Token(tokName, tokValue, rgxMtr.start(), rgxMtr.end());
+                    Token newTok = new Token(tokName, tokValue);
+//                    if (!tokName.equals("SPACE"))
+                        System.out.println(newTok);
+                    lxr.tokList.add(newTok);
+                }
+            }
+        }
+        if (tokCnt == 0) pos++;
+        strProcessor(lxr, strG.substring(pos));
     }
 }
+
     /*
 Ввод 000:
 go code with me
--- src/razrpop/DSL/flexer/Token.java
+++ src/razrpop/DSL/flexer/Token.java
@@ -3,18 +3,50 @@ package razrpop.DSL.flexer;
 public class Token {
     private String type;
     private String value;
+//    private int posL; // index
+//    private int posR;
 
     public Token(String type, String value) {
         this.type = type;
         this.value = value;
     }
 
-    public String getValue() {
-        return value;
-    }
+    // *
+//    public Token(String type, String value, int posL, int posR) {
+//        this.type = type;
+//        this.value = value;
+//        this.posL = posL;
+//        this.posR = posR;
+//    }
+//    public int getPosL() {
+//        return posL;
+//    }
+//    public void setPosL(int posL) {
+//        this.posL = posL;
+//    }
+//    public int getPosR() {
+//        return posR;
+//    }
+//    public void setPosR(int posR) {
+//        this.posR = posR;
+//    }
+//    @Override
+//    public String toString() {
+//        return "Token{" +
+//                "type='" + type + '\'' +
+//                ", value='" + value + '\'' +
+//                ", posL=" + posL +
+//                ", posR=" + posR +
+//                '}';
+//    }
+    // *
 
     public String getType() {
         return type;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     @Override
@@ -24,4 +56,5 @@ public class Token {
                 ", value='" + value + '\'' +
                 '}';
     }
+
 }
